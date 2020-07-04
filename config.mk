911c99c0b2ed7ce1e2d39258f5ce136817682a37
Daniel Moch <daniel@danielmoch.com>
2020-07-04 12:04:46 -0400

    Roll version

--- config.mk
+++ config.mk
@@ -7,7 +7,7 @@ DIST_SRC = ${SRC} Makefile config.mk xhotplug.1
 OBJ = ${SRC:.c=.o}
 INCS = -I${X11BASE}/include
 LIBS = -L${X11BASE}/lib -lxcb -lxcb-randr
-VERSION = 0.1.0dev0
+VERSION = 0.1.0
 
 CPPFLAGS := -DVERSION=\"${VERSION}\"
 CFLAGS := -std=c99 -pedantic-errors -Wall -Wextra -Werror -O2 -c ${INCS}
